49a841cf3c86e4923cf954daafaa3c5f076e1c54
HaHoangK16 <hoang.hathucminh@gmail.com>
2020-09-15 11:01:02 +0700

    Static Property

---
+++ StaticProperty/Car.cs
@@ -0,0 +1,40 @@
+namespace StaticProperty
+{
+    public class Car
+    {
+        private string name;
+        private string engine;
+        public static int numberOfCars;
+
+
+        public Car(string name, string engine)
+        {
+            this.name = name;
+            this.engine = engine;
+            numberOfCars++;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public string getEngine()
+        {
+            return engine;
+        }
+        public void setEngine(string engine)
+        {
+            this.engine= engine;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + this.name + " Engine: " + this.engine;
+        }
+    }
+}
