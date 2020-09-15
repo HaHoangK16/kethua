d86ea5b1126c3c4335b2d7dd4a402b618afdccc6
HaHoangK16 <hoang.hathucminh@gmail.com>
2020-09-15 11:23:44 +0700

    Xay dung Class Student

---
+++ ClassStudent/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(1, "M.Hoang", "Hue", 20);
+
+            Student student2 = new Student();
+            student2.SetId(2);
+            student2.SetName("N.Phuoc");
+            student2.SetAddress("Hue");
+            student2.SetAge(22);
+
+            Console.WriteLine(student1.ToString());
+            Console.WriteLine(student2.ToString());
+            Console.Read();
+        }
+    }
+}
