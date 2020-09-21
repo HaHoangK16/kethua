dcd1e81ffafe0edd23a176c13594fb55f47cbd32
HaHoangK16 <hoang.hathucminh@gmail.com>
2020-09-21 11:28:42 +0700

    Lop Point va MoveablePoint

---
+++ MoveablePoint/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+
+namespace MoveablePoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Point");
+            Point point = new Point(5.6f, 7.9f);
+
+
+            Console.WriteLine(point);
+            Console.WriteLine("---------------\n");
+
+            Console.WriteLine("MovablePoint");
+            MovablePoint movablePoint = new MovablePoint(8.2f, 20.6f);
+            Console.WriteLine(movablePoint.Move());
+            Console.WriteLine(movablePoint.ToString());
+        }
+    }
+}
