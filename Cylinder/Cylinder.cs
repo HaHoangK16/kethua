94d53c82831825772a825300c9b51ac3dce6ab7a
HaHoangK16 <hoang.hathucminh@gmail.com>
2020-09-21 11:39:58 +0700

    Lop Circle va lop Cylinder

---
+++ Cylinder/Cylinder.cs
@@ -0,0 +1,34 @@
+namespace Cylinder1
+{
+    public class Cylinder : Circle
+    {
+        private double Height;
+
+        public Cylinder(double _Height,double R, string Color):base(R,Color)
+        {
+            Height = _Height;
+        }
+        public Cylinder()
+        {
+
+        }
+
+        public void setHeight(double _Height)
+        {
+            Height = _Height;
+        }
+        public double getHeight()
+        {
+            return Height;
+        }
+        
+        public double Volume()
+        {
+            return Area * Height;
+        }
+        public override string ToString()
+        {
+            return"Height = "+ getHeight() + " The tich = "+ Volume() + base.ToString();
+        }
+    }
+}
